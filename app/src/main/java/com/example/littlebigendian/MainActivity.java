c8e4748e7eb5657b2f543b6fcc74c38c1083d944
Dsapalov <denis.sapalov@gmail.com>
2022-10-20 20:08:42 +0300

    Refactoring

--- app/src/main/java/com/example/littlebigendian/MainActivity.java
+++ app/src/main/java/com/example/littlebigendian/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         String littleEndian;
         String bigEndian;
         String hexValue;
+        int numberOfBytes;
     }
 
     ArrayList<Vector> vectors = new ArrayList<Vector>();
@@ -38,17 +39,19 @@ public class MainActivity extends AppCompatActivity {
     void startCalculation() {
 
         for (int i = 0; i < vectors.size(); i++) {
-            // 1 +
-//            hexToLittleEndian(vectors.get(i));
-            // 2
-            hexToBigEndian(vectors.get(i));
+            System.out.println(String.format("--------> ================ %d vector test ================", i));
+            String hexToLittleEndianResult = hexToLittleEndian(vectors.get(i));
+            System.out.println("--------> hexToLittleEndian " + hexToLittleEndianResult);
 
-            // 3 +
-//            littleEndianToHex(vectors.get(i));
-            // 4 +
-//            bigEndianToHex(vectors.get(i));
+            String hexToBigEndianResult = hexToBigEndian(vectors.get(i));
+            System.out.println("--------> hexToBigEndianResult " + hexToBigEndianResult);
+
+            String littleEndianToHexResult = littleEndianToHex(vectors.get(i));
+            System.out.println("--------> littleEndianToHexResult " + littleEndianToHexResult);
+
+            String bigEndianToHexResult = bigEndianToHex(vectors.get(i));
+            System.out.println("--------> bigEndianToHexResult " + bigEndianToHexResult);
         }
-
 
     }
 
@@ -56,29 +59,33 @@ public class MainActivity extends AppCompatActivity {
         Vector firstVector = new Vector();
         firstVector.bigEndian = "115339776388732929035197660848497720713218148788040405586178452820382218977280";
         firstVector.littleEndian = "255";
-        firstVector.hexValue = "ff00000000000000000000000000000000000000000000000000000000000000";
+        firstVector.hexValue = "0xff00000000000000000000000000000000000000000000000000000000000000";
+        firstVector.numberOfBytes = 32;
         vectors.add(firstVector);
 
         Vector secondVector = new Vector();
         secondVector.bigEndian = "77193548260167611359494267807458109956502771454495792280332974934474558013440";
         secondVector.littleEndian = "43690";
         secondVector.hexValue = "aaaa000000000000000000000000000000000000000000000000000000000000";
-//        vectors.add(secondVector);
+        secondVector.numberOfBytes = 32;
+        vectors.add(secondVector);
 
         Vector thirdVector = new Vector();
         thirdVector.bigEndian = "4294967295";
         thirdVector.littleEndian = "4294967295";
         thirdVector.hexValue = "FFFFFFFF";
-//        vectors.add(thirdVector);
+        thirdVector.numberOfBytes = 4;
+        vectors.add(thirdVector);
 
         Vector fourthVector = new Vector();
         fourthVector.bigEndian = "979114576324830475023518166296835358668716483481922294110218890578706788723335115795775136189060210944584475044786808910613350098299181506809283832360654948074334665509728123444088990750984735919776315636114949587227798911935355699067813766573049953903257414411690972566828795693861196044813729172123152193769005290826676049325224028303369631812105737593272002471587527915367835952474124875982077070337970837392460768423348044782340688207323630599527945406427226264695390995320400314062984891593411332752703846859640346323687201762934524222363836094053204269986087043470117703336873406636573235808683444836432453459818599293667760149123595668832133083221407128310342064668595954073131257995767262426534143159642539179485013975461689493733866106312135829807129162654188209922755829012304582671671519678313609748646814745057724363462189490278183457296449014163077506949636570237334109910914728582640301294341605533983878368789071427913184794906223657920124153256147359625549743656058746335124502376663710766611046750739680547042183503568549468592703882095207981161012224965829605768300297615939788368703353944514111011011184191740295491255291545096680705534063721012625490368756140460791685877738232879406346334603566914069127957053440";
         fourthVector.littleEndian = "240";
+        fourthVector.numberOfBytes = 255;
         fourthVector.hexValue = "F000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
-//        vectors.add(fourthVector);
+        vectors.add(fourthVector);
     }
 
-    void hexToLittleEndian(Vector inputVector) {
+    String hexToLittleEndian(Vector inputVector) {
         long ret = 0;
         String hexLittleEndian = "";
         if (inputVector.hexValue.length() % 2 == 0) {
@@ -92,34 +99,32 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        System.out.println("---> hexToLittleEndian result: " + ret);
+        return String.format("%d", ret);
     }
 
-    void hexToBigEndian(Vector inputVector) {
+    String hexToBigEndian(Vector inputVector) {
         long ret = 0;
-        String hexLittleEndian = "";
         if (inputVector.hexValue.length() % 2 == 0) {
             try {
-                ret = Long.parseLong("ff", 16);
+                ret = Long.parseLong(inputVector.hexValue, 16);
             } catch (Exception ex ){
                 System.out.println("---> parseLong ex: " + ex.toString());
             }
         }
 
-        System.out.println("---> hexToBigEndian result: " + ret);
+        return String.format("%d", ret);
     }
 
-    void littleEndianToHex(Vector inputVector) {
+    String littleEndianToHex(Vector inputVector) {
 
         BigInteger big = new BigInteger(inputVector.littleEndian);
-        String result = big.toString(16);
-        System.out.println("---> littleEndianToHex result: " + result);
+        return big.toString(16);
     }
 
-    void bigEndianToHex(Vector inputVector ) {
+    String bigEndianToHex(Vector inputVector ) {
         BigInteger big = new BigInteger(inputVector.bigEndian);
         String result = big.toString(16);
-        System.out.println("---> bigEndianToHex result: " + result);
+        return result;
     }
 
 
